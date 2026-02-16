0190d33fc597cfc7dbce44a02b429c3c2da6e7ee
jsra2025 <joses.ruizarizmendi@gmail.com>
2026-02-16 07:35:57 -0600

    modulo etiquetado recepcion

---
+++ addons/stock_label_custom/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Stock Label Custom',
+    'version': '1.0',
+    'author': 'QUIMIBOND',
+    'license': 'LGPL-3',
+    'depends': ['stock'],
+    'data': [
+        'report/stock_label_custom_pdf.xml',
+        'report/stock_label_custom_zpl.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
