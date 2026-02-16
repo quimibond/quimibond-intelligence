1ca746ec49b15827aa4c5a98fe5515d901c343bb
jsra2025 <joses.ruizarizmendi@gmail.com>
2026-02-16 15:16:05 -0600

    correcciones

--- addons/stock_dymo_labels/models/stock_picking.py
+++ addons/stock_dymo_labels/models/stock_picking.py
@@ -1,18 +1,14 @@
-from odoo import models, api
+from odoo import models, _
+from odoo.exceptions import UserError
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def action_print_dymo_labels(self):
-        """ 
-        Acción para imprimir etiquetas de las líneas de movimiento 
-        que tengan un número de lote asignado.
-        """
         self.ensure_one()
-        # Obtenemos las líneas de movimiento (move_line_ids) que tienen lote
+        # Filtramos las líneas que tengan lote (lot_id)
         move_lines = self.move_line_ids.filtered(lambda l: l.lot_id)
         if not move_lines:
-            # Opcional: Podrías lanzar una excepción si no hay lotes
-            return False
+            raise UserError(_("No hay líneas con lote asignado para imprimir."))
             
         return self.env.ref('stock_dymo_labels.action_report_dymo_picking_label').report_action(move_lines)
