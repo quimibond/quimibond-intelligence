293c17d05da207feb3670ce5c2e78ba6150587ad
jsra2025 <joses.ruizarizmendi@gmail.com>
2026-02-16 12:08:11 -0600

    opcion impresion en menu

--- addons/stock_label_custom/__manifest__.py
+++ addons/stock_label_custom/__manifest__.py
@@ -8,6 +8,7 @@
         'report/stock_label_custom_pdf.xml',
         'report/stock_label_custom_zpl.xml',
         'report.xml',
+        'report/report_label_layout.xml',
     ],
     'installable': True,
     'application': False,
